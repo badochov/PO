a50e72413eea064e05e497b848b97afc29294321
Hubert Badocha <badocha.hubert@gmail.com>
2020-04-29 15:44:47 +0200

    lab8

--- Laby/l7/src/badocha/hubert/Nauczyciel.java
+++ Laby/l7/src/badocha/hubert/Nauczyciel.java
@@ -1,4 +1,18 @@
 package badocha.hubert;
 
-public class Nauczyciel {
+public class Nauczyciel extends Pracownik {
+    private String ulubionyPrzedmiot;
+
+    Nauczyciel(String imię, String nazwisko, String pracodawca, String ulubionyJezyk) {
+        super(imię, nazwisko, pracodawca);
+
+        this.ulubionyPrzedmiot = ulubionyJezyk;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + " > " + "Nauczyciel{" +
+                "ulubionyPrzedmiot='" + ulubionyPrzedmiot + '\'' +
+                '}';
+    }
 }
